704a21d92435f7cc9f38bdd3b48e85691bdd98a2
Nishchit Bhandari <nbhellossss@gmail.com>
2024-10-06 05:01:02 +0545

    Fix: CORS issue

--- api/create_app.py
+++ api/create_app.py
@@ -1,8 +1,15 @@
 from fastapi import FastAPI
 from .routes import mainpage, plantrec
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI()
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Replace with specific origins in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 app.include_router(mainpage.route)
 app.include_router(plantrec.route)
